eb27059d2bca4c08724934145af33236f782f609
du <15638931035@163.com>
2026-05-24 22:08:30 +0800

    fix: disable WebView caching in MainActivity.java to avoid old asset caching issues

--- android/app/src/main/java/com/ruankao/quiz/MainActivity.java
+++ android/app/src/main/java/com/ruankao/quiz/MainActivity.java
@@ -1,7 +1,22 @@
 // author du
 package com.ruankao.quiz;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 中文注释：获取WebView并设置其不使用缓存，保证每次启动拉取最新页面
+        WebView webView = this.bridge.getWebView();
+        if (webView != null) {
+            WebSettings settings = webView.getSettings();
+            settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        }
+    }
+}
+
 
